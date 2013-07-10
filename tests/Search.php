98e89e6c61ebb7e744bb06da89e4433200df99a9
FreebieStock <team@freebievectors.com>
2013-07-10 02:20:26 +0100

    Unnecesary line

--- tests/Search.php
+++ tests/Search.php
@@ -4,7 +4,6 @@ include dirname(__DIR__) . '/lib/Quandl.php';
 $settings = include dirname(__DIR__) . '/settings.php';
 $token = $settings->token;
 
-$ticker = 'DOE/RWTC';
 $quandl = new Quandl($token);
 $data = $quandl->search('oil');
 $data = json_decode($data);
